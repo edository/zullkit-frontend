cc34da267c063507169f8c92dae3c2c7dd2dfb8e
Mohammad Aminullah <mohaminullah91@gmail.com>
2022-04-01 13:25:25 +0700

    Update dari pertemuan 9

--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import ItemsCard from './../ItemsCard.vue';
+import ItemCard from './../ItemCard.vue';
 
 const items = ref([
   { id: 1, title: 'Mobile UI Kit', image: 'items-1.jpg', caption: 'Mobile UI Kit' },
@@ -13,7 +13,7 @@ const items = ref([
   <div class="container px-4 mx-auto my-16 md:px-12">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      <ItemsCard
+      <ItemCard
         v-for="item in items"
         :key="item.id"
         :title="item.title"
