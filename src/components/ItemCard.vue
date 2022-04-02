e475971820d6c82b13ab483664b61807a3c1bd02
Mohammad Aminullah <mohaminullah91@gmail.com>
2022-04-02 19:22:28 +0700

    Update 1 April

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -2,6 +2,7 @@
 import { RouterLink } from "vue-router";
 
 defineProps({
+  id: Number,
   title: String,
   image: String,
   caption: String
@@ -12,7 +13,7 @@ defineProps({
 <template>
   <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
     <div class="overflow-hidden border border-gray-200 rounded-xl">
-      <RouterLink to="/">
+      <RouterLink :to="'/product/' + id">
         <div class="m-4 overflow-hidden rounded-xl">
           <img :alt="title" class="block w-full h-auto" :src="'/src/assets/img/' + image" />
         </div>
@@ -21,8 +22,9 @@ defineProps({
       <header class="px-4 mb-4 leading-tight">
         <h1 class="text-lg">
           <RouterLink
+            :to="'/product/' + id"
             class="font-semibold text-black no-underline hover:underline"
-            to="/"
+            to
           >{{ title }}</RouterLink>
         </h1>
         <span class="block text-sm font-light text-gray-500 no-underline">{{ caption }}</span>
