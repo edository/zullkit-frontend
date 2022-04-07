77f698f3c7b820bf0346e51fc914212a8b02e91f
Mohammad Aminullah <mohaminullah91@gmail.com>
2022-04-07 13:36:55 +0700

    5 April 2022

--- src/components/homepage/Categories.vue
+++ src/components/homepage/Categories.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+
 import CategoryCard from './../CategoryCard.vue'
+
 
 const categories = ref([
   { id: 1, title: 'Mobile UI Kit', count: 731, image: 'categories-1.jpg' },
@@ -8,10 +10,15 @@ const categories = ref([
   { id: 3, title: 'Icon Set', count: 83559, image: 'categories-3.jpg' },
   { id: 4, title: 'Website UI Kit', count: 4500, image: 'categories-4.jpg' }
 ])
+
+onMounted(() => {
+
+})
+
 </script>
 
 <template>
-  <div class="container px-4 mx-auto my-16 md:px-12">
+  <div class="container px-4 mx-auto my-16 md:px-12" id="categories">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
       <CategoryCard
--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 import Gallery from '@/components/detail/Gallery.vue';
 
 </script>
@@ -95,10 +96,10 @@ import Gallery from '@/components/detail/Gallery.vue';
                 </li>
               </ul>
             </div>
-            <a
-              href="checkout.html"
+            <RouterLink
+              to="/pricing"
               class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow"
-            >Download Now</a>
+            >Download Now</RouterLink>
           </div>
         </div>
       </aside>
